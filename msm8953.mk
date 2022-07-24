03ca42edc742b4c71804aaaf18e81b0f7a609d2b
zhantech <hifzhan41@gmail.com>
2022-07-24 13:02:02 +0000

    Nuke LiveDisplay

--- msm8953.mk
+++ msm8953.mk
@@ -236,9 +236,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     lights.msm8953
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
